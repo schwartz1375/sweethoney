554afcb842821804fadeed33e07a3cb73b90e70f
schwartz1375 <matthew.r.schwartz@gmail.com>
2020-03-02 21:23:07 -0500

    added fuzzy hashing, and alerts on suspicious IAT

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ Sweethoney is a file triaging tool.  The tool parses out the following informati
 * Lists functions declared and referenced
 * Lists exported symbols
 * Calculates file statics
+* Alerts on suspicious IAT - This is static list, and it maybe need to be tuned for your use case!
 
 For golang binaries please look at the [Go Reverse Engineering Tool Kit](https://go-re.tk/)
--- sweethoney.py
+++ sweethoney.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 __author__ = 'Matthew Schwartz (@schwartz1375)'
-__version__ = '0.8'
+__version__ = '0.9'
 
 import hashlib
 import time
@@ -27,6 +27,17 @@ try:
 except:
     print('Missing termcolor Python module, please check if it is installed.') #pip install termcolor
     sys.exit(1)
+
+try:
+    import ssdeep
+except:
+    print('Missing ssdeep Python module, please check if it is installed.') #python3 -m pip install ssdeep
+    sys.exit(1)
+
+# suspicious APIs to alert on 
+alerts = ['AdjustTokenPrivileges', 'OpenProcess', 'VirtualAllocEx', 'WriteProcessMemory', 'CreateRemoteThread', 'ReadProcessMemory',
+          'CreateProcess', 'WinExec', 'ShellExecute', 'HttpSendRequest', 'InternetReadFile', 'InternetConnect',
+          'CreateService', 'StartService']
 
 def Main(filename):
     print("PE check for '%s':" % filename)
@@ -57,10 +68,24 @@ def Main(filename):
     cprint("***************************************", 'blue')
     cprint("Functions declared and referenced:", 'blue')
     cprint("***************************************", 'blue')
-    for entry in pe.DIRECTORY_ENTRY_IMPORT:
-        print (str(entry.dll, 'utf-8'))
-    for function in entry.imports:
-       print ("\t" + str(function.name , 'utf-8'))
+    ret = []
+    for lib in pe.DIRECTORY_ENTRY_IMPORT:
+        print (str(lib.dll, 'utf-8'))
+        for imp in lib.imports:
+            if imp.name != None:
+                print ('\t' + str(imp.name, 'utf-8'))
+            if (imp.name != None) and (imp.name != ""):
+                for alert in alerts:
+                    #print("\t Alert: " + alert)
+                    if imp.name.decode('utf-8').startswith(alert):
+                        ret.append(imp.name)
+    if len(ret) != 0:
+        cprint("***************************************", 'red')
+        cprint("Suspicious IAT alerts", 'red', attrs=['bold'])
+        cprint("***************************************", 'red')
+        #print(*ret, sep=', ')
+        for x in ret:
+            cprint("\t"+x.decode("utf-8"), 'red', attrs=['bold'])
 
     cprint("***************************************", 'blue')
     cprint("Looking for exported sysmbols...", 'blue')
@@ -70,7 +95,7 @@ def Main(filename):
             #print hex(pe.OPTIONAL_HEADER.ImageBase + exp.address), exp.name, exp.ordinal
             print("Name: %s, Ordinal number: %i" % (str(exp.name, 'utf-8'), exp.ordinal))
     except:
-        cprint("No exported symbols!", 'red')
+        cprint("No exported symbols!", 'magenta')
 
     cprint("***************************************", 'blue')
     cprint("Getting Sections...", 'blue')
@@ -98,16 +123,17 @@ def Main(filename):
     print('SHA-256 hash: %s' % hashlib.sha256(raw).hexdigest())
     print('SHA-512 hash: %s' % hashlib.sha512(raw).hexdigest())
     print('Import hash (imphash): %s' % pe.get_imphash())
-    sys.exit()
+    print('fuzzy hash (ssdeep): %s' % ssdeep.hash_from_file(filename))
     #print("List all PE headers")
     #pprint.pprint(dir(pe))
+    sys.exit()
 
 def getFileType(filename):
     filetype = magic.from_file(filename)
     cprint("***************************************", 'blue')
-    cprint("Geting filetype...", 'blue')
+    cprint("Getting filetype...", 'blue')
     cprint("***************************************", 'blue')
-    cprint(filetype, 'magenta')
+    cprint(filetype, 'green')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='A rapid file analysis tool')
